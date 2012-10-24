85cd743f7dd2f5f6aca9c224df7d30f035d7edfb
Martin Nagy <code@nagy.no>
2012-10-24 23:14:28 +0200

    Init commit

---
+++ masterAR/masterAR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  masterAR
+//
+//  Created by Martin Nagy on 10/24/12.
+//  Copyright (c) 2012 Martin Nagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ masterAR/masterARTests/masterARTests.h
@@ -0,0 +1,13 @@
+//
+//  masterARTests.h
+//  masterARTests
+//
+//  Created by Martin Nagy on 10/24/12.
+//  Copyright (c) 2012 Martin Nagy. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface masterARTests : SenTestCase
+
+@end
