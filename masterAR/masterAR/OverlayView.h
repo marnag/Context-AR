23347882cbbcc64adf3d39188eb9d74318832212
Martin Nagy <code@nagy.no>
2012-12-15 17:07:30 +0100

    Added overlay OpenGL view

---
+++ masterAR/masterAR/OverlayView.h
@@ -0,0 +1,14 @@
+//
+//  OverlayView.h
+//  masterAR
+//
+//  Created by Martin Nagy on 12/15/12.
+//  Copyright (c) 2012 Martin Nagy. All rights reserved.
+//
+
+#ifndef masterAR_OverlayView_h
+#define masterAR_OverlayView_h
+
+
+
+#endif
---
+++ masterAR/masterAR/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  OverlayViewController.h
+//  masterAR
+//
+//  Created by Martin Nagy on 12/15/12.
+//  Copyright (c) 2012 Martin Nagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
