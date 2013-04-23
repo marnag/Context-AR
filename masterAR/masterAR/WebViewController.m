8434153c858b8c625611c30d93600c82a6c9106a
Martin Nagy <code@nagy.no>
2013-04-23 15:40:54 +0200

    Diverse

---
+++ masterAR/masterAR/WebViewController.m
@@ -0,0 +1,38 @@
+//
+//  WebViewController.m
+//  masterAR
+//
+//  Created by Martin Nagy on 1/9/13.
+//  Copyright (c) 2013 Martin Nagy. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
